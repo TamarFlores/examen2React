f8dd67c51e0bac9a7141b30e5235b4b65c0c54b5
Tamar <tamiflore1@gmail.com>
2021-03-05 01:36:34 -0400

    examen2 TamarFlores

---
+++ src/components/ToDo.js
@@ -0,0 +1,99 @@
+import React from 'react';
+import {StyleSheet, View, Text, TouchableHighlight } from 'react-native';
+
+import Request from '../server/Request.js';
+import { UPDATE_TAREA_DONE} from '../server/EndPoints.js';
+
+class ToDo extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            done: this.props.done
+        }
+    }
+
+    cambioEstado(id){
+        var request = new Request('put', UPDATE_TAREA_DONE + '/' + id, {}, (response) => {
+          if(response.status == 200){
+            this.setState({
+              done: true
+            })
+          }
+        });
+        request.start();
+    }
+
+    render(){
+        const fecha = new Date(this.props.date).toLocaleDateString();
+        return (
+            <View style={styles.tarea}>
+                <Text style={styles.textoHora}>{this.props.hour}</Text>
+
+                <View>
+                    <Text style={this.state.done ? styles.tituloTareaCompletado : styles.tituloTarea}>{this.props.name}</Text>
+                    <Text style={styles.textoDescripcion}>{this.props.description}</Text>
+                    <Text style={styles.textoFecha}>{fecha}</Text>
+                </View>
+                <View>
+                    <TouchableHighlight style={this.state.done? styles.btnCambiarEstadoDisabled : styles.btnCambiarEstado} 
+                                    onPress={() => this.cambioEstado(this.props.id)} 
+                                    disabled={this.state.done}>
+                        <Text style={styles.textoBtnCambiarEstado}>COMPLET</Text>
+                    </TouchableHighlight>
+                </View>
+            
+             </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    tarea: {
+        backgroundColor: 'white',
+        padding: 15,
+        borderRadius: 9,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        marginVertical: 6
+    },
+    tituloTarea: {
+        fontWeight: 'bold',
+        fontSize: 18,
+        color: '#2E4053'
+    },
+    tituloTareaCompletado: {
+        fontWeight: 'bold',
+        fontSize: 17,
+        color: '#2E4053',
+        textDecorationLine: 'line-through'
+    },
+    textoHora: {
+        color: '#B2BABB',
+        fontSize: 16
+    },
+    textoDescripcion: {
+        color: '#B2BABB',
+        fontSize: 16,
+        fontWeight: 'bold'
+    },
+    textoFecha: {
+        color: '#B2BABB',
+        fontSize: 13,
+    },
+    btnCambiarEstado: {
+        backgroundColor: 'cornflowerblue',
+        padding: 9,
+        borderRadius: 7
+    },
+    btnCambiarEstadoDisabled: {
+        backgroundColor: '#7F8C8D',
+        padding: 9,
+        borderRadius: 7
+    },
+    textoBtnCambiarEstado: {
+        color: 'white'
+    }
+});
+
+export default ToDo;
---
+++ src/server/EndPoints.js
@@ -0,0 +1,2 @@
+export const GET_TAREA = 'http://172.20.0.2:8000/tarea';
+export const UPDATE_TAREA_DONE = 'http://172.20.0.2:8000/tarea/done';
